6762ffcb34044b34b1be125aba6c7f6a9c97148d
mdjco <mdjco@Heimdall.Heimdall>
2024-11-02 21:56:37 +0200

    WIP: adding set to maintain distinct set of data between nodes
    Currently not working, I think I have an issue with parsing my data at
    some points, will need to check how the data comes back from maelstrom
    (check the .(int) parsing)

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module broadcast
 go 1.23.2
 
 require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240813160128-8b9e94c75e59
+
+require github.com/deckarep/golang-set/v2 v2.6.0 // indirect
--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
+	mapset "github.com/deckarep/golang-set/v2"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
+
+type Container struct {
+	mu   sync.Mutex
+	data mapset.Set[int]
+}
+
+func (c *Container) addData(data int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data.Add(data)
+}
+
+func (c *Container) addDataSlice(data []int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data.Append(data...)
+}
 
 func main() {
 	log.Println("starting node")
 
-	var messages = []any{}
+	var container = Container{}
 
 	n := maelstrom.NewNode()
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
@@ -22,8 +43,21 @@ func main() {
 			return err
 		}
 
-		message := body["message"]
-		messages = append(messages, message)
+		message := body["message"].(int)
+		container.addData(message)
+
+		data := container.data.ToSlice()
+		sharedResponse := map[string]any{
+			"type": "shared_data",
+			"data": data,
+		}
+
+		for _, node := range n.NodeIDs() {
+			if node == n.ID() {
+				continue
+			}
+			n.Send(node, sharedResponse)
+		}
 
 		response := map[string]any{
 			"type": "broadcast_ok",
@@ -32,13 +66,27 @@ func main() {
 		return n.Reply(msg, response)
 	})
 
+	n.Handle("shared_data", func(msg maelstrom.Message) error {
+		log.Println("received data to propagate")
+
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		data := body["data"].([]int)
+		container.addDataSlice(data)
+
+		return nil
+	})
+
 	n.Handle("read", func(msg maelstrom.Message) error {
 		log.Println("handling read message")
 
 		// Do you have to empty the messages after a read? Doesn't explicitly say so?
 		response := map[string]any{
 			"type":     "read_ok",
-			"messages": messages,
+			"messages": container.data,
 		}
 
 		return n.Reply(msg, response)
@@ -53,6 +101,8 @@ func main() {
 
 		return n.Reply(msg, response)
 	})
+
+	// Handle writing the data into the messages array,
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
