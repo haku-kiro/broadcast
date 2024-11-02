820fdfe1ba5f3fef6b48421899ead9d3618f30d8
mdjco <mdjco@Heimdall.Heimdall>
2024-11-02 20:30:07 +0200

    Working solution for basic test
    This works as long as there is a single node, adding a new node breaks
    the test as there is no data shared between the nodes.

--- justfile
+++ justfile
@@ -2,11 +2,13 @@ default:
 	@just --list
 
 build:
-	echo "todo: add the build path for you node"
+	go install .
 
 test: build
-	echo "Update this to point to the binary when you're done"
-	maelstrom test -w broadcast --bin ~/go/bin/<node name> --node-count 1 --time-limit 20 --rate 10
+	maelstrom test -w broadcast --bin ~/go/bin/broadcast --node-count 1 --time-limit 20 --rate 10
+
+heavy-test: build
+	maelstrom test -w broadcast --bin ~/go/bin/broadcast --node-count 2 --time-limit 20 --rate 10
 
 # Starts up the maelstrom server which shows detailed results.
 results:
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -9,19 +10,51 @@ import (
 func main() {
 	log.Println("starting node")
 
+	var messages = []any{}
+
 	n := maelstrom.NewNode()
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		log.Println("handling broadcast message")
-		return nil
+
+		// deserialize the message, handle data message retrieve
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		message := body["message"]
+		messages = append(messages, message)
+
+		response := map[string]any{
+			"type": "broadcast_ok",
+		}
+
+		return n.Reply(msg, response)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
 		log.Println("handling read message")
-		return nil
+
+		// Do you have to empty the messages after a read? Doesn't explicitly say so?
+		response := map[string]any{
+			"type":     "read_ok",
+			"messages": messages,
+		}
+
+		return n.Reply(msg, response)
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
 		log.Println("handling topology message")
-		return nil
+
+		response := map[string]any{
+			"type": "topology_ok",
+		}
+
+		return n.Reply(msg, response)
 	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
