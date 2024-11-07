05aaf15253043ad9c568a1bf7a1acfbea1b51ec0
mdjco <mdjco@Heimdall.Heimdall>
2024-11-07 20:13:16 +0200

    WIP: trying to figure out how to better detect network issues

--- justfile
+++ justfile
@@ -4,17 +4,26 @@ default:
 build:
 	go install .
 
-test: build
+clear-logs:
+	rm -rf /tmp/log.json
+
+error-logs:
+	@cat /tmp/log.json | rg 'err' | jq '.'
+
+logs:
+	@cat /tmp/log.json | jq '.'
+
+test: build clear-logs
 	maelstrom test -w broadcast --bin ~/go/bin/broadcast --node-count 1 --time-limit 20 --rate 10
 
-quick-test: build
+quick-test: build clear-logs
 	# Testing by creating a network partition
 	maelstrom test -w broadcast --bin ~/go/bin/broadcast --node-count 2 --time-limit 10 --rate 10 --nemesis partition
 
-test-multi-node: build
+test-multi-node: build clear-logs
 	maelstrom test -w broadcast --bin ~/go/bin/broadcast --node-count 5 --time-limit 30 --rate 20
 
-test-fault-tolerance: build
+test-fault-tolerance: build clear-logs
 	maelstrom test -w broadcast --bin ~/go/bin/broadcast --node-count 5 --time-limit 20 --rate 10 --nemesis partition
 
 # Starts up the maelstrom server which shows detailed results.
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"log/slog"
+	"os"
 	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -56,9 +58,10 @@ func newRetry(n *maelstrom.Node, tries int) *retry {
 }
 
 type server struct {
-	nodeId string
-	node   *maelstrom.Node
-	r      *retry
+	nodeId  string
+	node    *maelstrom.Node
+	r       *retry
+	context context.Context
 
 	mu   sync.RWMutex
 	data map[int]struct{}
@@ -91,15 +94,25 @@ func (s *server) broadcastHandler(msg maelstrom.Message) error {
 	s.data[id] = struct{}{}
 	s.mu.Unlock()
 
+	slog.Info("sending to all nodes")
 	for _, node := range s.node.NodeIDs() {
-		if node == s.nodeId {
+		if node == s.node.ID() {
 			continue
 		}
 
-		s.r.action(message{
-			destination: node,
-			body:        body,
-		})
+		slog.Info("sending to node", "node", node, "from", s.node.ID())
+
+		resultMessage, err := s.node.SyncRPC(s.context, node, msg)
+		if err != nil {
+			slog.Error("error when trying to broadcast to other nodes", "error", err)
+			return err
+		}
+		slog.Info("result message", "data", resultMessage)
+
+		// s.r.action(message{
+		// 	destination: node,
+		// 	body:        body,
+		// })
 	}
 
 	return s.node.Reply(msg, map[string]any{
@@ -124,21 +137,33 @@ func (s *server) topologyHandler(msg maelstrom.Message) error {
 
 func NewServer(n *maelstrom.Node, r *retry) *server {
 	return &server{
-		nodeId: n.ID(),
-		node:   n,
-		mu:     sync.RWMutex{},
-		r:      r,
-		data:   make(map[int]struct{}),
+		nodeId:  n.ID(),
+		node:    n,
+		mu:      sync.RWMutex{},
+		context: context.Background(),
+		r:       r,
+		data:    make(map[int]struct{}),
 	}
 }
 
 func main() {
+	logFile, err := os.OpenFile("log.json", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	logger := slog.New(slog.NewJSONHandler(logFile, nil))
+	slog.SetDefault(logger)
+
+	slog.Info("creating node")
 	n := maelstrom.NewNode()
 	r := newRetry(n, 10)
 	defer r.close()
 
+	slog.Info("creating server")
 	server := NewServer(n, r)
 
+	slog.Info("adding handlers")
 	n.Handle("broadcast", server.broadcastHandler)
 	n.Handle("read", server.readHandler)
 	n.Handle("topology", server.topologyHandler)
